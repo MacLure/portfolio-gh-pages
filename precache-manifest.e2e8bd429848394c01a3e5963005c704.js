7dceccb69bb55e4c51b42d2d908b08296dd15cbf
Malcolm <malcolm.maclure@gmail.com>
2019-03-29 19:56:57 -0400

    Updates

---
+++ precache-manifest.e2e8bd429848394c01a3e5963005c704.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e9f8d23e27af0cd0c67850a4f31f00d",
+    "url": "/portfolio-gh-pages/static/media/postgres.2e9f8d23.svg"
+  },
+  {
+    "revision": "d7cae65bb54b88a98194",
+    "url": "/portfolio-gh-pages/static/css/main.95684628.chunk.css"
+  },
+  {
+    "revision": "4234d05dccaec7349443",
+    "url": "/portfolio-gh-pages/static/js/runtime~main.61e167d2.js"
+  },
+  {
+    "revision": "38da1f0b27c02a4fc8e4",
+    "url": "/portfolio-gh-pages/static/js/2.05927c29.chunk.js"
+  },
+  {
+    "revision": "6694eb9773e1cf2d2d5b17601a911870",
+    "url": "/portfolio-gh-pages/static/media/rails.6694eb97.svg"
+  },
+  {
+    "revision": "07b7d168c9877537a56595caa1710564",
+    "url": "/portfolio-gh-pages/static/media/html5.07b7d168.svg"
+  },
+  {
+    "revision": "194669ccf69b0310a49a73ae0545f915",
+    "url": "/portfolio-gh-pages/static/media/css3.194669cc.svg"
+  },
+  {
+    "revision": "73c1e4150f8bb5fc95f349a564215318",
+    "url": "/portfolio-gh-pages/static/media/js.73c1e415.svg"
+  },
+  {
+    "revision": "39d8ea45ac494aa65be267c39daa0416",
+    "url": "/portfolio-gh-pages/static/media/ruby.39d8ea45.svg"
+  },
+  {
+    "revision": "d7cae65bb54b88a98194",
+    "url": "/portfolio-gh-pages/static/js/main.884bbfbe.chunk.js"
+  },
+  {
+    "revision": "ee96964e6c90374f7c764d71e13e5c24",
+    "url": "/portfolio-gh-pages/static/media/portrait.ee96964e.png"
+  },
+  {
+    "revision": "de2c16ff89280e314621029be140113d",
+    "url": "/portfolio-gh-pages/static/media/sass.de2c16ff.svg"
+  },
+  {
+    "revision": "9a74994ab5414e11dde0cf37b9f7ed1b",
+    "url": "/portfolio-gh-pages/static/media/react.9a74994a.svg"
+  },
+  {
+    "revision": "2cd6a6f5cea96354fa9ee322843d6d06",
+    "url": "/portfolio-gh-pages/static/media/redux.2cd6a6f5.svg"
+  },
+  {
+    "revision": "daded3e2bd885d39a82837506f132678",
+    "url": "/portfolio-gh-pages/static/media/horizonScreencap.daded3e2.png"
+  },
+  {
+    "revision": "fba504785175916ee6e0c3787b3c7ffe",
+    "url": "/portfolio-gh-pages/static/media/reserveItScreencap.fba50478.png"
+  },
+  {
+    "revision": "865bab1710ad50cab0f92fe759d5b32f",
+    "url": "/portfolio-gh-pages/static/media/gramophoneCutout.865bab17.png"
+  },
+  {
+    "revision": "e77fe7459295fb4f85a37d22ff3c692b",
+    "url": "/portfolio-gh-pages/index.html"
+  }
+];
